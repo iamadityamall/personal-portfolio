4af6090b55012a3122fc214f1a6c2f28d0aaa36c
iamadityamall <contact.adityamall@gmail.com>
2022-03-21 03:27:09 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,14 @@ const Footer = () => {
   return (
     <footer className="h-auto bg-primary flex items-center justify-between border-t-2 border-ternary xl:justify-center xl:space-x-20">
       {/* footer title */}
-      <div className="text-md py-6 font-Poppins font-semibold">
+      <div className="text-sm py-6 font-Poppins font-semibold lg:text-lg">
         <p>designed with 🛠️, 💻 and ☕ by Aditya | ©️ {new Date().getFullYear()}</p>
       </div>
 
       {/* social icons */}
       <a
         href="https://www.linkedin.com/in/adityaharendramall/"
-        className="text-2xl py-6"
+        className="text-xl py-6 lg:text-2xl"
         target="_blank"
         rel="noreferrer"
       >
