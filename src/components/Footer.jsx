23c68c9bfd73e95b0231f8618944ceda170d8fa7
iamadityamall <contact.adityamall@gmail.com>
2022-03-21 03:30:12 +0530

    text size updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="h-auto bg-primary flex items-center justify-between border-t-2 border-ternary xl:justify-center xl:space-x-20">
       {/* footer title */}
-      <div className="text-sm py-6 font-Poppins font-semibold lg:text-lg">
+      <div className="text-xs py-6 font-Poppins font-semibold lg:text-lg">
         <p>designed with 🛠️, 💻 and ☕ by Aditya | ©️ {new Date().getFullYear()}</p>
       </div>
 
