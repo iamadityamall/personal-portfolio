c666a0942bb6a2c93710e6f53bcc9a0e96c1f068
iamadityamall <contact.adityamall@gmail.com>
2022-03-22 00:25:26 +0530

    mobile project card completed

--- src/components/Home/ProjectsSection.jsx
+++ src/components/Home/ProjectsSection.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import ProjectsCard from "../ProjectsCard";
+import CategoryCard from "../CategoryCard";
+
 
 const ProjectsSection = () => {
   return (
-    <div className='h-screen bg-yellow-400'id="projects">ProjectsSection</div>
-  )
-}
+    <section
+      className="min-h-screen bg-primary flex flex-col items-center text-center space-y-8 p-2"
+      id="projects"
+    >
+      <div className="font-Poppins flex flex-col space-y-2">
+        <h1 className="text-2xl p-3">Projects</h1>
+        <p className="p-4 ">
+          "It takes half your life before you discover life is a do-it-yourself
+          project"
+        </p>
+      </div>
+      {/* Portfolio projects */}
+      <div className="flex flex-col space-y-3">
+        <ProjectsCard />
+      </div>
 
-export default ProjectsSection
+      {/* some more projects */}
+      <div className="flex flex-col space-y-3">
+        <div className="font-Poppins flex flex-col space-y-2">
+          <h1 className="text-2xl p-3">Other Projects</h1>
+          <p className="p-4 ">
+            "Every project is an opportunity to learn, to figure out problems
+            and challenges, to invent and reinvent"
+          </p>
+        </div>
+        <CategoryCard />
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsSection;
---
+++ src/components/ProjectsCard.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import ProfilePicture from "../images/portfolio-website.png";
+import { FaHtml5 } from "react-icons/fa";
+import { VscGithubInverted } from "react-icons/vsc";
+import { BiCodeAlt } from "react-icons/bi";
+
+const ProjectsCard = () => {
+  return (
+    <article className="bg-secondary flex flex-col rounded-3xl overflow-auto">
+      {/* image container */}
+      <header className="w-full h-[300px] p-2">
+        <img
+          src={ProfilePicture}
+          alt=""
+          className="w-full h-full object-cover rounded-3xl"
+        />
+      </header>
+      {/* project info */}
+      <div className="text-left flex flex-col space-y-4 font-Poppins p-6">
+        <div className="flex flex-col space-y-2">
+          <h1 className="py-2 text-xl text-darkPurple">Portfolio Page</h1>
+          <p className="text-black/70">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            voluptatibus consectetur ea at temporibus aut veniam architecto
+            consequatur quam ratione.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <h1 className="py-2 text-xl text-darkPurple">
+            What Problem it solves
+          </h1>
+          <p className="text-black/70">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            possimus cupiditate fugiat voluptatum? Voluptatem possimus
+            architecto perspiciatis, voluptatibus ratione repellendus?
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <h1 className="py-2 text-xl text-darkPurple">Features</h1>
+          <p className="text-black/70">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            possimus cupiditate fugiat voluptatum? Voluptatem possimus
+            architecto perspiciatis, voluptatibus ratione repellendus?
+          </p>
+        </div>
+      </div>
+      {/* footer */}
+      <div className="flex flex-col justify-between bg-darkPurple/10 items-center">
+        <div className="px-6 text-2xl flex space-x-2 w-full justify-between">
+          <div className="flex items-center space-x-4">
+            <VscGithubInverted />
+            <BiCodeAlt />
+          </div>
+          <div className="px-3 py-2">2020</div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default ProjectsCard;
