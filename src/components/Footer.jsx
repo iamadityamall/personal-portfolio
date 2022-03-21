eca0bb53c5dcb281615ecbe438d76567adc763c0
iamadityamall <contact.adityamall@gmail.com>
2022-03-21 22:48:26 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="h-auto bg-primary flex items-center justify-between border-t-2 border-ternary sm:justify-center sm:space-x-20">
+    <footer className="h-auto bg-primary flex space-x-10 items-center justify-center border-t-2 border-ternary sm:justify-center sm:space-x-20">
       {/* footer title */}
       <div className="text-xs py-6 font-Poppins font-semibold sm:text-lg">
         <p>designed with 🛠️, 💻 and ☕ by Aditya | ©️ {new Date().getFullYear()}</p>
