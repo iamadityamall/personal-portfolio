de380d8cf0dc4b1fe4a9e238b72f45d1da980cf0
iamadityamall <contact.adityamall@gmail.com>
2022-03-22 12:55:55 +0530

    update project card

--- src/components/Home/ProjectsSection.jsx
+++ src/components/Home/ProjectsSection.jsx
@@ -6,18 +6,18 @@ import CategoryCard from "../CategoryCard";
 const ProjectsSection = () => {
   return (
     <section
-      className="min-h-screen bg-primary flex flex-col items-center text-center space-y-8 p-2"
+      className="min-h-screen bg-primary flex flex-col items-center text-center space-y-8 p-2 lg:p-12 lg:space-y-12"
       id="projects"
     >
-      <div className="font-Poppins flex flex-col space-y-2">
-        <h1 className="text-2xl p-3">Projects</h1>
-        <p className="p-4 ">
+      <div className="font-Poppins flex flex-col lg:flex-row lg:justify-end lg:space-x-10 lg:items-center lg:w-full">
+        <h1 className="text-2xl p-3 lg:text-5xl">projects-📝</h1>
+        <p className="p-4 lg:text-2xl">
           "It takes half your life before you discover life is a do-it-yourself
           project"
         </p>
       </div>
       {/* Portfolio projects */}
-      <div className="flex flex-col space-y-3">
+      <div className="flex flex-col space-y-3 ">
         <ProjectsCard />
       </div>
 
--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -5,9 +5,9 @@ import { BiCodeAlt } from "react-icons/bi";
 
 const ProjectsCard = () => {
   return (
-    <article className="bg-secondary flex flex-col rounded-3xl overflow-auto">
+    <article className="bg-secondary flex flex-col rounded-3xl overflow-auto lg:flex-row lg:items-center">
       {/* image container */}
-      <header className="w-full h-[300px] p-2">
+      <header className="w-full h-[300px] p-2 lg:h-full lg:p-3">
         <img
           src={ProfilePicture}
           alt=""
@@ -15,44 +15,48 @@ const ProjectsCard = () => {
         />
       </header>
       {/* project info */}
-      <div className="text-left flex flex-col space-y-4 font-Poppins p-6">
+      <div className="text-left flex flex-col space-y-4 font-Poppins p-4">
         <div className="flex flex-col space-y-2">
-          <h1 className="py-2 text-xl text-darkPurple">Portfolio Page</h1>
-          <p className="text-black/70 text-sm">
+          <h1 className="py-2 text-xl text-darkPurple lg:text-2xl">
+            Portfolio Page
+          </h1>
+          <p className="text-black/70 text-sm lg:text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
             voluptatibus consectetur ea at temporibus aut veniam architecto
             consequatur quam ratione.
           </p>
         </div>
         <div className="flex flex-col space-y-2">
-          <h1 className="py-2 text-xl text-darkPurple">
+          <h1 className="py-2 text-xl text-darkPurple lg:text-2xl">
             What Problem it solves
           </h1>
-          <p className="text-black/70 text-sm">
+          <p className="text-black/70 text-sm lg:text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
             possimus cupiditate fugiat voluptatum? Voluptatem possimus
             architecto perspiciatis, voluptatibus ratione repellendus?
           </p>
         </div>
         <div className="flex flex-col space-y-2">
-          <h1 className="py-2 text-xl text-darkPurple">Features</h1>
-          <p className="text-black/70 text-sm">
+          <h1 className="py-2 text-xl text-darkPurple lg:text-2xl">Features</h1>
+          <p className="text-black/70 text-sm lg:text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
             possimus cupiditate fugiat voluptatum? Voluptatem possimus
             architecto perspiciatis, voluptatibus ratione repellendus?
           </p>
         </div>
-      </div>
-      {/* footer */}
-      <div className="flex flex-col justify-between bg-darkPurple/10 items-center">
-        <div className="px-6 text-xl flex space-x-2 w-full justify-between">
-          <div className="flex items-center space-x-4">
-            <VscGithubInverted />
-            <BiCodeAlt />
+        <div className="flex flex-col justify-between bg-darkPurple/10 items-center rounded-xl">
+          <div className="px-6 text-sm flex space-x-6 w-full justify-center items-center p-2 lg:text-lg lg:justify-start">
+            <div>html css tailwind</div>
+            <div className="flex items-center text-sm space-x-4 lg:text-lg">
+              <VscGithubInverted className="cursor-pointer" />
+              <BiCodeAlt className="cursor-pointer" />
+            </div>
+            <div className="">2020</div>
           </div>
-          <div className="px-3 py-2">2020</div>
         </div>
       </div>
+
+      {/* footer */}
     </article>
   );
 };
