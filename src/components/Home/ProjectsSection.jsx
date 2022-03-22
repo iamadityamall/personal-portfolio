c1d91a7a3c9d02ffd30ba4bbd22c5a7677fca8eb
iamadityamall <contact.adityamall@gmail.com>
2022-03-22 14:22:45 +0530

    updated hero section and about section

--- src/components/Home/ProjectsSection.jsx
+++ src/components/Home/ProjectsSection.jsx
@@ -6,16 +6,17 @@ import CategoryCard from "../CategoryCard";
 const ProjectsSection = () => {
   return (
     <section
-      className="min-h-screen bg-primary flex flex-col items-center text-center space-y-8 p-2 lg:p-12 lg:space-y-12"
+      className="min-h-screen bg-primary flex flex-col items-center text-center space-y-8 p-2 xl:p-12 xl:space-y-12"
       id="projects"
     >
-      <div className="font-Poppins flex flex-col lg:flex-row lg:space-x-10 lg:items-center lg:justify-center lg:w-full">
-        <h1 className="text-2xl p-3 lg:text-5xl">projects-📝</h1>
-        <p className="p-4 lg:text-2xl">
+      <div className="font-Poppins flex flex-col xl:flex-row xl:space-x-10 xl:items-center xl:justify-center xl:w-full">
+        <h1 className="text-2xl p-3 xl:text-5xl">projects-📝</h1>
+        <p className="p-4 xl:text-2xl">
           "It takes half your life before you discover life is a do-it-yourself
           project"
         </p>
       </div>
+
       {/* Portfolio projects */}
       <div className="flex flex-col space-y-3 ">
         <ProjectsCard />
--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -5,49 +5,51 @@ import { BiCodeAlt } from "react-icons/bi";
 
 const ProjectsCard = () => {
   return (
-    <article className="bg-secondary flex flex-col rounded-3xl overflow-auto lg:flex-row lg:items-center shadow-xl">
+    <article className="bg-secondary flex flex-col rounded-xl overflow-auto xl:flex-row xl:items-center shadow-xl p-2 xl:h-[75vh] xl:w-auto">
+
       {/* image container */}
-      <div className="w-full h-[300px] p-2 lg:h-full lg:p-3">
+      <div className="w-full h-[300px] xl:h-full p-2">
         <img
           src={ProfilePicture}
           alt=""
-          className="w-full h-full object-cover rounded-3xl"
+          className="w-full h-full object-cover rounded-xl"
         />
       </div>
+
       {/* project info */}
-      <div className="text-left flex flex-col space-y-4 font-Poppins p-4 lg:w-3/4">
-        <div className="flex flex-col space-y-2">
-          <h1 className="py-2 text-xl text-darkPurple lg:text-xl border-b-2 border-darkPurple">
+      <div className="text-left flex flex-col space-y-4 font-Poppins p-4 xl:w-3/4 xl:h-full xl:justify-center">
+        <div className="flex flex-col">
+          <h1 className="py-2 text-xl text-darkPurple xl:text-lg border-b-2 border-darkPurple">
             Portfolio Page
           </h1>
-          <p className="text-black/70 text-sm lg:text-lg">
+          <p className="text-black/70 text-sm xl:text-sm py-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
             voluptatibus consectetur ea at temporibus aut veniam architecto
             consequatur quam ratione.
           </p>
         </div>
-        <div className="flex flex-col space-y-2">
-          <h1 className="py-2 text-xl text-darkPurple lg:text-xl">
+        <div className="flex flex-col">
+          <h1 className="py-2 text-xl text-darkPurple xl:text-lg">
             What Problem it solves?
           </h1>
-          <p className="text-black/70 text-sm lg:text-lg">
+          <p className="text-black/70 text-sm xl:text-sm py-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
             possimus cupiditate fugiat voluptatum? Voluptatem possimus
             architecto perspiciatis, voluptatibus ratione repellendus?
           </p>
         </div>
-        <div className="flex flex-col space-y-2">
-          <h1 className="py-2 text-xl text-darkPurple lg:text-xl">Features:</h1>
-          <p className="text-black/70 text-sm lg:text-lg">
+        <div className="flex flex-col">
+          <h1 className="py-2 text-xl text-darkPurple xl:text-lg">Features:</h1>
+          <p className="text-black/70 text-sm xl:text-sm py-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
             possimus cupiditate fugiat voluptatum? Voluptatem possimus
             architecto perspiciatis, voluptatibus ratione repellendus?
           </p>
         </div>
-        <div className="flex flex-col justify-between bg-darkPurple/10 items-center rounded-xl">
-          <div className="px-6 text-sm flex space-x-6 w-full justify-center items-center p-2 lg:text-lg lg:justify-start">
+        <div className="flex flex-col justify-between bg-darkPurple/10 items-center rounded-xl xl:w-4/6">
+          <div className="px-6 text-xs flex space-x-6 w-full justify-center items-center p-2 xl:justify-start xl:w-full">
             <div>html css tailwind</div>
-            <div className="flex items-center text-sm space-x-4 lg:text-lg">
+            <div className="flex items-center text-sm space-x-4 xl:text-lg">
               <VscGithubInverted className="cursor-pointer" />
               <BiCodeAlt className="cursor-pointer" />
             </div>
