274c27e804b40544f0a8ab06e79a5a22b2075a78
iamadityamall <contact.adityamall@gmail.com>
2022-03-25 23:10:33 +0530

    social section updated

--- src/components/Home/SocialSection.jsx
+++ src/components/Home/SocialSection.jsx
@@ -5,13 +5,15 @@ import SocialCards from '../SocialCards'
 const SocialSection = () => {
   return (
     <section id="social" className='border-t-2 border-darkPurple space-y-10 flex flex-col h-screen justify-center'>
+
         {/* heading and info */}
         <div className='flex flex-col items-center text-center font-Poppins p-4'>
             <h1 className='text-xl p-2 xl:text-5xl border-b-2 border-darkPurple'>code and design presence</h1>
             <p className='xl:text-xl xl:p-5'>"look at my work on other platforms. caution: you might not find many, since started recently."</p>
         </div>
+
         {/* map through links */}
-        <div className='grid grid-col-1 gap-y-4 px-4 xl:grid xl:grid-cols-4 xl:gap-8 xl:w-[80vw] xl:mx-auto'>
+        <div className='grid grid-col-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid lg:grid-cols-4'>
             {socialLinks.map((socialLink) => {
                 const {id} = socialLink;
                 return <SocialCards key={id} {...socialLink}/>
