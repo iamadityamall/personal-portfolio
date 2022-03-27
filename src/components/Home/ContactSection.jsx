3f9adc87945bc6abdaed75ef3e9650c307ca3733
iamadityamall <contact.adityamall@gmail.com>
2022-03-27 16:15:05 +0530

    contact page updated

--- src/components/Home/ContactSection.jsx
+++ src/components/Home/ContactSection.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import SubmitButton from "../SubmitButton";
+import { useState } from "react";
 
 const ContactSection = () => {
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  });
+
+  const postData = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
   return (
     <article
       className="h-screen flex flex-col space-y-4 justify-center border-t-2 border-darkPurple bg-primary font-Poppins xl:flex-row xl:items-center xl:justify-evenly"
@@ -9,17 +26,26 @@ const ContactSection = () => {
     >
       {/*  form heading*/}
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-2xl p-2 xl:text-5xl xl:p-6 xl:border-b-2 xl:border-darkPurple">contact-me-☎️</h1>
-        <p className="xl:text-xl xl:p-6">drop a message, let's connect</p>
+        <h1 className="text-2xl p-2 xl:text-5xl xl:p-6 xl:border-b-2 xl:border-darkPurple">
+          contact-me-☎️
+        </h1>
+        <p className="xl:text-xl xl:p-6">drop a message, let's connect. fueled with firebase</p>
       </div>
       <div>
-        <form name="contact" method="POST" data-netlify="true" className="flex flex-col w-[350px] mx-auto space-y-2 xl:w-[400px]">
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          className="flex flex-col w-[350px] mx-auto space-y-2 xl:w-[400px]"
+        >
           <input
             type="text"
             name="firstName"
             id=""
             placeholder="first name"
             className="p-4 border-2 border-darkPurple"
+            value={userData.firstName}
+            onChange={(e) => postData(e)}
           />
           <input
             type="text"
@@ -27,6 +53,8 @@ const ContactSection = () => {
             id=""
             placeholder="last name"
             className="p-4 border-2 border-darkPurple"
+            value={userData.lastName}
+            onChange={(e) => postData(e)}
           />
           <input
             type="email"
@@ -34,6 +62,8 @@ const ContactSection = () => {
             id="email"
             placeholder="email"
             className="p-4 border-2 border-darkPurple"
+            value={userData.email}
+            onChange={(e) => postData(e)}
           />
           <textarea
             name="message"
@@ -42,8 +72,14 @@ const ContactSection = () => {
             rows="10"
             placeholder="type your message"
             className="p-4 border-2 border-darkPurple"
+            value={userData.message}
+            onChange={(e) => postData(e)}
           ></textarea>
-          <SubmitButton name="<send/>" />
+          <SubmitButton
+            name="<send/>"
+            {...userData}
+            setUserData={setUserData}
+          />
         </form>
       </div>
     </article>
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
+import React from "react";
 
-const SubmitButton = ({name}) => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(e)
+const SubmitButton = ({
+  name,
+  firstName,
+  lastName,
+  email,
+  message,
+  setUserData,
+}) => {
+  const submitData = async (e) => {
+    e.preventDefault();
+    if (firstName && lastName && email && message) {
+      const res = fetch(
+        "https://personal-portfolio-contact-default-rtdb.firebaseio.com/userDataRecords.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            firstName,
+            lastName,
+            email,
+            message,
+          }),
+        }
+      );
+      if (res) {
+        setUserData({
+          firstName: "",
+          lastName: "",
+          email: "",
+          message: "",
+        });
+        alert("data stored");
+      }
+    } else {
+      alert("please fill the data");
     }
+  };
 
   return (
-    <button type='submit' className='bg-secondary p-2 rounded-lg text-darkPurple border-2 border-darkPurple' onSubmit={handleSubmit}>{name}</button>
-  )
-}
+    <button
+      type="submit"
+      className="bg-secondary p-2 rounded-lg text-darkPurple border-2 border-darkPurple"
+      onClick={(e) => submitData(e)}
+    >
+      {name}
+    </button>
+  );
+};
 
-export default SubmitButton
+export default SubmitButton;
