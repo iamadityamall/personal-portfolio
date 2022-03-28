12aaa247698e4ca0ae66f79c459a1c6fa383f6f3
iamadityamall <contact.adityamall@gmail.com>
2022-03-28 12:34:49 +0530

    vertical email section

--- src/components/Home/VerticalEmailSection.jsx
+++ src/components/Home/VerticalEmailSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const VerticalEmailSection = () => {
   return (
-    <div className='xl:-rotate-90 xl:top-[55%] xl:left-[-15.7%] xl:fixed'>
+    <div className='xl:-rotate-90 xl:fixed xl:left-[-235px] xl:top-[390px]'>
       <div className="flex items-center space-x-5 justify-center">
         <div className="h-0.5 w-32 bg-lightPurple"></div>
         <p className="font-Poppins text-darkPurple">contact.adityamall@gmail.com</p>
